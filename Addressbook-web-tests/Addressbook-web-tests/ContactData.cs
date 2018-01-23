d2dfb696c67b4ae38644f0605f750c1bc56ab9ae
Daria Shishkova_ <daria.shishkova@t-systems.com_>
2018-01-23 15:41:49 +0300

    Created classes ContactCreationTests and ContactData

---
+++ Addressbook-web-tests/Addressbook-web-tests/ContactData.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string middlename;
+        private string lastname;
+        private string nickname;
+        private string title;
+        private string company;
+        private string address1;
+        private string homeTel1;
+        private string mobileTel;
+        private string workTel;
+        private string fax;
+        private string email1;
+        private string email2;
+        private string email3;
+        private string homepage;
+        private string address2;
+        private string homeTel2;
+        private string notes;
+
+        public ContactData(string firstname)
+        {
+            this.firstname = firstname;
+        }
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        public string Middlename
+        {
+            get { return middlename; }
+            set { middlename = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public string Nickname
+        {
+            get { return nickname; }
+            set { nickname = value; }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+        public string Address1
+        {
+            get { return address1; }
+            set { address1 = value; }
+        }
+        public string HomeTel1
+        {
+            get { return homeTel1; }
+            set { homeTel1 = value; }
+        }
+        public string MobileTel
+        {
+            get { return mobileTel; }
+            set { mobileTel = value; }
+        }
+        public string WorkTel
+        {
+            get { return workTel; }
+            set { workTel = value; }
+        }
+        public string Fax
+        {
+            get { return fax; }
+            set { fax = value; }
+        }
+        public string Email1
+        {
+            get { return email1; }
+            set { email1 = value; }
+        }
+        public string Email2
+        {
+            get { return email2; }
+            set { email2 = value; }
+        }
+        public string Email3
+        {
+            get { return email3; }
+            set { email3 = value; }
+        }
+        public string Homepage
+        {
+            get { return homepage; }
+            set { homepage = value; }
+        }
+        public string Address2
+        {
+            get { return address2; }
+            set { address2 = value; }
+        }
+        public string HomeTel2
+        {
+            get { return homeTel2; }
+            set { homeTel2 = value; }
+        }
+        public string Notes
+        {
+            get { return notes; }
+            set { notes = value; }
+        }
+    }
+    }
