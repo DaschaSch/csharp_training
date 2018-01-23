35dff49a2d176c92759f98a23a91836c49fc02ec
Daria Shishkova_ <daria.shishkova@t-systems.com_>
2018-01-23 13:21:32 +0300

    Added classes LoginData and GroupData Updated method GroupCreationTest

--- Addressbook-web-tests/Addressbook-web-tests/GroupCreationTests.cs
+++ Addressbook-web-tests/Addressbook-web-tests/GroupCreationTests.cs
@@ -45,24 +45,53 @@ namespace WebAddressbookTests
         [Test]
         public void GroupCreationTest()
         {
-            driver.Navigate().GoToUrl(baseURL + "addressbook/");
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("q");
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("q");
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("q");
-            driver.FindElement(By.Name("submit")).Click();
+            OpenLoginPage();
+            Login(new LoginData("admin", "secret"));
+            OpenGroupPage();
+            GroupData newgroup =new GroupData("aaa");
+            newgroup.GroupHeader = "ddd";
+            newgroup.GroupFooter = "ccc";
+            CreateGroup(newgroup);
+            Logout();
+        }
+
+        private void Logout()
+        {
             driver.FindElement(By.LinkText("group page")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
         }
+
+        private void CreateGroup(GroupData newgroup)
+        {
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(newgroup.GroupName);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(newgroup.GroupHeader);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(newgroup.GroupFooter);
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void OpenGroupPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void Login(LoginData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private void OpenLoginPage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/");
+        }
+
         private bool IsElementPresent(By by)
         {
             try
---
+++ Addressbook-web-tests/Addressbook-web-tests/GroupData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class GroupData
+    {
+        private string gname;
+        private string gheader;
+        private string gfooter;
+
+        public GroupData(string gname)
+        {
+            this.gname = gname;
+        }
+
+        public GroupData(string gname, string gheader, string gfooter)
+        {
+            this.gname = gname;
+            this.gheader = gheader;
+            this.gfooter = gfooter;
+        }
+
+        public string GroupName
+        {
+            get { return gname; }
+            set { gname = value; }
+        }
+        public string GroupHeader
+        {
+            get { return gheader; }
+            set { gheader = value; }
+        }
+        public string GroupFooter
+        {
+            get { return gfooter; }
+            set { gfooter = value; }
+        }
+
+    }
+}
